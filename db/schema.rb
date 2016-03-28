49c838c623c426c07178631a9fec02bdabed2eb9
Brit Butler <redline6561@gmail.com>
2016-03-28 10:52:07 -0400

    Write and run migration to give posts a user_id.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328141738) do
+ActiveRecord::Schema.define(version: 20160328142511) do
 
   create_table "post_tags", force: :cascade do |t|
     t.integer  "tag_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160328141738) do
     t.datetime "written_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "tags", force: :cascade do |t|
