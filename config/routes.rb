af000b73eee5c6860f5cbcc469c3ed026588c76d
Brit Butler <redline6561@gmail.com>
2016-03-28 11:09:27 -0400

    Add new routes for posts.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   get "/", to: "posts#index"
+  get "posts/new", to: "posts#new"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
