0b4d0d5f003ccaedf4f7c8d46b535ba660f2b132
Brit Butler <redline6561@gmail.com>
2016-03-28 11:47:49 -0400

    Write a basic method to create posts in the database.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,10 @@ class PostsController < ApplicationController
   def new
     render :new
   end
+
+  def create
+    new_post = Post.create(title: params["title"],
+                           content: params["content"])
+    redirect_to "/"
+  end
 end
