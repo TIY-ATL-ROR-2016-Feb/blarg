61fce680c755305dcad2f58f973f1598c1cb142d
Brit Butler <redline6561@gmail.com>
2016-03-29 11:54:14 -0400

    Update controller to handle failed saves from the validation correctly.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,22 @@
 class PostsController < ApplicationController
   def index
-    posts = Post.all
-    render :index, locals: { posts: posts }
+    @posts = Post.all
+    render :index
   end
 
   def new
+    @post = Post.new
     render :new
   end
 
   def create
-    new_post = Post.create(title: params["title"],
-                           content: params["content"])
-    redirect_to :root
+    @post = Post.new(title: params["title"],
+                     content: params["content"])
+    if @post.save
+      redirect_to :root
+    else
+      render :new
+    end
   end
 
   def edit
