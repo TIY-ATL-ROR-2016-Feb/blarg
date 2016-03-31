a4e76b2c7adf5bbd133afc7073e812c20b33fc22
Brit Butler <redline6561@gmail.com>
2016-03-31 10:52:24 -0400

    Add a basic User model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330132202) do
+ActiveRecord::Schema.define(version: 20160331144940) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 20160330132202) do
 
   create_table "tags", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
