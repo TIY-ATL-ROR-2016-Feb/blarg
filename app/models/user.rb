96bbb1153364b3d4f8950b55035a61454bd64958
Brit Butler <redline6561@gmail.com>
2016-03-31 11:06:30 -0400

    Add some basic User validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  validates_presence_of :password, :email
+  # validates :password, format: { with: /[A-Z]{1,}[a-z][3,][0-9][1,]/,
+  #                                message: "must be at least "}
+  validates :password, length: { minimum: 8 }
+  validates :email, format: {
+              with: /.+@.+\..+/,  message: "Must have an @ symbol and a period."
+            }
 end
