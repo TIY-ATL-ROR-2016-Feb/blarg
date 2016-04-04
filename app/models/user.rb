48e37d90da727856c7500fc9f6197263bcbe5467
Brit Butler <redline6561@gmail.com>
2016-04-04 10:59:18 -0400

    Switch to bcrypt for password storage.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  validates_presence_of :password, :email
+  has_secure_password
+  validates_presence_of :email
   # validates :password, format: { with: /[A-Z]{1,}[a-z][3,][0-9][1,]/,
   #                                message: "must be at least "}
-  validates :password, length: { minimum: 8 }
   validates :email, format: {
               with: /.+@.+\..+/,  message: "Must have an @ symbol and a period."
             }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331161139) do
+ActiveRecord::Schema.define(version: 20160404145616) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
@@ -45,9 +45,10 @@ ActiveRecord::Schema.define(version: 20160331161139) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.string   "passwords"
   end
 
 end
